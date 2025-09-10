bd992c7315992535fce925ad3c9f45be3a64e402
Kazin7 <krustm8@naver.com>
2025-09-10 20:54:06 +0900

    [Fix] UIManager 기능 수정

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    [SerializeField] private InventoryUI inventoryUI;
     [SerializeField] private Sprite testItemSprite; // 임시 아이템
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E)) {
-            inventoryUI.AddItem(testItemSprite);
+            Manager.UI.InventoryUI.AddItem(testItemSprite);
         }
     }
 }
